243a7a0b47846f3cfcc92cfeb8075f448c5b0d9b
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-21 09:20:25 -0300

    Project created

---
+++ src/main/java/com/educandoweb/springBoot_udemy/SpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springBoot_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/course_java_udemy/CourseJavaUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course_java_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJavaUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
