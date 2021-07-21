d8d4b65830e06528e27b09f9a224bd7f86cbcfbd
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-21 09:42:37 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/springBoot_udemy/entities/User.java
+++ src/main/java/com/educandoweb/springBoot_udemy/entities/User.java
@@ -2,14 +2,26 @@ package com.educandoweb.springBoot_udemy.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String name;
+
 	private String email;
+
 	private String phone;
+
 	private String password;
 
 	public User() {
