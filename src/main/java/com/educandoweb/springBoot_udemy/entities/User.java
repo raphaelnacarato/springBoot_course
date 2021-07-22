dbd31254a134be70bcaf3c5a306313c2ac7574e4
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-22 10:43:37 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/springBoot_udemy/entities/User.java
+++ src/main/java/com/educandoweb/springBoot_udemy/entities/User.java
@@ -1,13 +1,18 @@
 package com.educandoweb.springBoot_udemy.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -23,6 +28,9 @@ public class User implements Serializable {
 	private String phone;
 
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -75,6 +83,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
