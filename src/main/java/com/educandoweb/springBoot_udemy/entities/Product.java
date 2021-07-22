b12571d2a94574b97379133f92b9784839cb1836
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-22 11:26:37 -0300

    Product

--- src/main/java/com/educandoweb/springBoot_udemy/entities/Product.java
+++ src/main/java/com/educandoweb/springBoot_udemy/entities/Product.java
@@ -12,8 +12,8 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -23,16 +23,25 @@ public class Category implements Serializable {
 
 	private String name;
 
-	@Transient
-	private Set<Product> products = new HashSet<>();
+	private String description;
 
-	public Category() {
+	private Double price;
+
+	private String imgUrl;
+
+	@Transient
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -51,8 +60,32 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -71,7 +104,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
